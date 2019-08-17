f94e44dbadea6b22a9ae71b8664bff929711aca4
louisvno <louisvn@gmail.com>
2019-08-17 12:44:05 +0200

    copy index

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,11 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CopyPlugin([
+      { from: './src/index.html', to: './index.html' },
+    ]),
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
