badcfe330d7e9b9367953cc9adcdfe9004fc4260
louisvno <louisvn@gmail.com>
2019-08-17 12:23:21 +0200

    bundle gpx

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import Leaflet from '../lib/leaflet_1.5.1/leaflet';
+import Leaflet from '../lib/leaflet_1.5.1/leaflet-src';
 import '../lib/leaflet_1.5.1/leaflet.css';
 import gpxParser from '../lib/gpx-parser/gpx-parser';
-window.onload = function init() {
-    fetch("track_20190728_082434_caldes_de_montbui.gpx")
-    .then(res => res.text())
-    .then(res => {
-      let parser = new gpxParser() //properties copied
-      parser.parse(res); 
-      // Load paths
-      let arrLatLong = parser.tracks[0].points.map(point => [point.lat, point.lon]);
-      Leaflet.polyline(arrLatLong, {color: 'red'})
-             .addTo(this.map);
-    })
-  
+import route from './assets/tracks/track_20190728_082434_caldes_de_montbui.gpx';
 
-  this.map = Leaflet.map('mapid')
+(function init() {
+  let parser = new gpxParser() //properties copied
+  parser.parse(route); 
+  // Load paths
+  let arrLatLong = parser.tracks[0].points.map(point => [point.lat, point.lon]);
+  let map = Leaflet.map('mapid')
     .setView([41.5, 2.14], 10);
-
+  Leaflet.polyline(arrLatLong, {color: 'red'})
+    .addTo(map);
   // Load map
   Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18,
-  }).addTo(this.map);
-};
+  }).addTo(map);
+})()
     
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
@@ -25,6 +25,12 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
          'file-loader'
+        ]
+      },
+      {
+        test: /\.gpx$/,
+        use: [
+          'raw-loader'
         ]
       }
     ],
